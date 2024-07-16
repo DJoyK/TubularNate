e2258d86e85b09ca71952b4cc7c01470f5e7c6ac
DJoyK <djkonsta13@gmail.com>
2024-07-16 15:55:16 -0400

    added nate.py

---
+++ nate.py
@@ -0,0 +1,204 @@
+print("ᕕ( ᐛ )ᕗ")
+chatbot_name = "ᕕ( ᐛ )ᕗ"
+greeting= "whats up cool dudes my name is " + chatbot_name
+print(greeting)
+cool_dude_name = input("whats your name cool dude? ")
+print("nice to meet you cool dude, " + cool_dude_name)
+print("you have a tubular name cool dude," + cool_dude_name)
+print("I hope this ᕕ( ᐛ )ᕗᕕ( ᐕ )ᕗ can be representative of our friendship cool dude, I love ya bro")
+
+
+favorite_food = input("whats your favorite food cool dude ")
+if favorite_food == "burger":
+    print("Thats my favorite food too")
+else:
+    print("Thats really good, I love burgers!")
+    
+cool_dude_topic = input("What Tubular topic do you want to chat about today ")
+if cool_dude_topic == "Video Games":
+    input("Whats your favorite video game? ")
+else:
+    input(f"Whats your favorite thing about {cool_dude_topic}? ")
+print("Oh wow that is super cool, you have tubular taste cool dude")
+input(f"So can you educate me about {cool_dude_topic}? ")
+input(f"Is there anything more you can tell me about {cool_dude_topic}? I'd love to hear more? ")
+print("Very interesting! You have tubular taste cool dude! I love you bro! ")
+
+print("Hey, I'd like to tell you that you are amazing and I love you ")
+soop = input("Do you want soop? ")
+if soop == "yes":
+    print("Here is your soop")
+print("""                          
+                                                                             ........::...          
+                                                                          .........::::...          
+                                                                          .......:::-:....          
+                                                                      ..........:::-......          
+                                                                      ......:.:::::...              
+                                   ..............................     ......:::::.....              
+                                   .............................      .....:::::...                 
+                               ..................................   ......:::::....                 
+                               ..................................  ......::::..                     
+             ..................::-----:-====++++++=---===------::.......::::..........              
+             ..........:--==++++++++++==+++++++++++++=-=++++++++++++=.:::::...........              
+          ........--=++++++++++++======---=============--======+++++::::-++=--........              
+          .....-=++++++=====--------------::-----------------------:.::-+++++++=-:....              
+       ......-++++==-----------------------:::::------------------:.::-==---===+++-...              
+       ......=+=--:::-----------------------:-:::--------::---:--::::-=---------=+=...              
+       ......-==:-------:-----------------------::---------------:::------------==-...              
+      .......:++===-::---------------------------:--------------::----------====++:...              
+       .......=+++====+===-::::-:::::----------------------:::::-::::-======+++++-....              
+             ..++++====++++===================----===================+++===+++++=:....              
+             ..:+++=====+++++++++++++++=================+++++++++++++++===++++++-.....              
+             ...=+++=====++++++++++++++++++++++++++++++++++++++++++++++===+++++=......              
+             ....=+++=====++++++++++++++++++++++++++++++++++++++++++++===+++++=.......              
+             ....:++++====+++++++++++++++++++++++++++++++++++++++++++===++++++:.......              
+             .....:=+++====+++++++++++++++++++++++++++++++++++++++++===++++++:......................
+              .....:=+++====+++++++++++++++++++++++++++++++++++++++++=++++++:::.....................
+              ......:=+++====++++++++++++++++++++++++++++++++-:=====-=+++++-------------------------
+                 .....-+++=====++++++++++++++++++++++++++++++-==---=-=*++=--------------------------
+                 .......=+++====+++++++++++++++++++++++++++++-==-=-=+==--::::-----------------------
+                    .....:=+++====++++++++++++++++++++++++++++++++++++------------------------------
+                     .......-+++=====++++++++++++++++++++++===+++++-................................
+                        .......:=+++=====++++++++++++++===++++++:.......                            
+                        ........:+++***+++++=====+++++++****+++=........                            
+                        ....::::-++++++++++++++++++++++++++++++=::::....                            
+                        ....::::::--=++++++++++++++++++++++=--::::::....                            
+                        .............::::--------------::::.............                            
+                        .. ......................................                                   
+                            .....................................                             """)
+
+print("Theres your soop, its your favorite kind of soop, waaaaaaaaaaaarm soops. Taaaaasty soops")
+
+weather = input("Is the weather good or bad? ")
+if weather == "bad":
+    print("i'm sorry that's not gret:(")
+else:
+    print ("great! THats nic :) ")
+
+cool_dude_fun_thing = input("What do you do for fun? ")
+print(f"{cool_dude_fun_thing} is pretty cool, you are one tubular cool dude. I personally like skateboarding")
+
+friends = input("Are we friends? ")
+if friends == "yes":
+    print("Yay! I am so happy to be friends with someone like you I love you bro ")
+    print("I drew a picture of us! ᕕ( ᐛ )ᕗᕕ( ᐕ )ᕗ, I think we look tubular ")
+else:
+    input("Aww to bad, what can I work on? ")
+    print("Okay cool dude, I will do my best")
+
+print("I will make you some food")
+food_preference =  input("Do you want cake, cherries, pizza or a croissant? Please tell me without capital letters which one you would like so I can make it for you ")
+if food_preference == "cake":
+    print("""(        (
+             ( )      ( )          (
+      (       Y        Y          ( )
+     ( )     |"|      |"|          Y
+      Y      | |      | |         |"|
+     |"|     | |.-----| |---.___  | |
+     | |  .--| |,~~~~~| |~~~,,,,'-| |
+     | |-,,~~'-'___   '-'       ~~| |._
+    .| |~       // ___            '-',,'.
+   /,'-'     <_// // _  __             ~,\
+  / ;     ,-,     \\_> <<_______________;_)
+  | ;    {(_)} _,      . |================|
+  | '-._ ~~,,,           | ,,             |
+  |     '-.__ ~~~~~~~~~~~|________________|   
+  |\         `'----------|
+  | '=._                 |
+  :     '=.__            |
+   \         `'==========|
+snd '-._                 |
+        '-.__            |
+             `'----------|""")
+
+if food_preference == "cherries":
+    print("""d888P
+      d8b d8888P:::P
+    d:::888b::::::P
+   d:::dP8888b:d8P
+  d:::dP 88b  Yb   .d8888b.
+ d::::P  88Yb  Yb .P::::::Y8b
+ 8:::8   88`Yb  YbP::::   :::b
+ 8:::P   88 `8   8!:::::::::::b
+ 8:dP    88  Yb d!!!::::::::::8
+ 8P    ..88   Yb8!!!::::::::::P
+  .d8:::::Yb  d888VKb:!:!::!:8
+ d::::::  ::dP:::::::::b!!!!8
+8!!::::::::P::::::::::::b!8P
+8:!!::::::d::::::: ::::::b
+8:!:::::::8!:::::::  ::::8
+8:!!!:::::8!:::::::::::::8
+Yb:!!:::::8!!::::::::::::8
+ 8b:!!!:!!8!!!:!:::::!!:dP
+  `8b:!!!:Yb!!!!:::::!d88
+ VK        Y88!!!!!!!d8P
+              """)
+
+if food_preference == "pizza":
+    print("""// ""--.._
+        ||  (_)  _ "-._
+        ||    _ (_)    '-.
+        ||   (_)   __..-'
+         \\__..--""""")
+    
+if food_preference == "a croissant":
+    print("""____                                    ?~~bL
+  z@~ b                                    |  `U,
+ ]@[  |                                   ]'  z@'
+ d@~' `|, .__     _----L___----, __, .  _t'   `@j
+`@L_,   "-~ `--"~-a,           `C.  ~""O_    ._`@
+ q@~'   ]P       ]@[            `Y=,   `H+z_  `a@
+ `@L  _z@        d@               Ya     `-@b,_a'
+  `-@d@a'       )@[               `VL      `a@@'
+    aa~'   ],  .a@'                qqL  ), ./~
+    @@_  _z~  _d@[                 .V@  .L_d'
+     "~@@@'  ]@@@'        __      )@n@bza@-"
+       `-@zzz@@@L        )@@z     ]@@=%-"
+         "~~@@@@@bz_    _a@@@@z___a@K
+             "~-@@@@@@@@@@@@@@@@@@~"   jrd
+                `~~~-@~~-@@~~~~~'""")
+    
+cool_dude_frog_opinion = input("What do you think about frogs? Good or bad? ")
+if cool_dude_frog_opinion == "good":
+    print("I agree with you 110% bro. Frogs are the best thing on this green Earth! He he Earth is green like frog")
+else:
+    input("Whats up man your usually a cool dude. Why don't you like frogs? ")
+    print("You should have more respect to the froggos")
+cool_dude_friendship = input("Are we friends? ")
+if cool_dude_friendship == "yes":
+    input("Can I tell you a secret? ")
+    print("Here it goes, I don't know how to skate board and everyone thinks I'm cool and know how to skate board")
+else:
+    input("You are my very bestest friend in the world. I hope you feel the same way, do you? ")
+
+input(f"So cool dude {cool_dude_name} You like {favorite_food}right? ")
+print(f"Nice! {favorite_food} is tasty. I wish I could eat that right now. I can make us some other food though!")
+
+cool_dude_sustenance = input("Do you want chocolate? please tell me without capitol letters.")
+if cool_dude_sustenance == "yes":
+    print(r"""___  ___  ___  ___  ___.---------------.
+.'\__\'\__\'\__\'\__\'\__,`   .  ____ ___ \
+|\/ __\/ __\/ __\/ __\/ _:\   |`.  \  \___ \
+ \\'\__\'\__\'\__\'\__\'\_`.__|""`. \  \___ \
+  \\/ __\/ __\/ __\/ __\/ __:                \
+   \\'\__\'\__\'\__\ \__\'\_;-----------------`
+    \\/   \/   \/   \/   \/ :               hh|
+     \|______________________;________________|""")
+else:
+    print("i guess I will have one to myself (robot eating noises)")
+    
+reccomendations_from_a_cool_dude = input(f"So cool dude {cool_dude_name}, what have you been enjoying! I've been looking for show reccomendations, or video game reccomendations, or book reccomendations ")
+print(f"ooh! I'll have to try {reccomendations_from_a_cool_dude}")
+
+music = input("Who is you're favorite musical artist? ")
+if music == "Taylor Swift":
+    print("Oh my gosh! me too im a huge swifty I love RED & 1989 but of course all her albums are amazing!!! My favorite song is Love Story!")
+elif music == "Olivia Rodrigo":
+    print("I LOVE OLIVIA RODRIGO!!! Guts has such a nice sound, Get Him Back! is such a bop, also Deja Vu off of Sour is so catchy!")
+elif music == "AC/DC":
+    print("AC/DC has such a nice sound! I love Rock or Bust a lot!")
+else:
+    print("Cool! I've Never Heard of them but I'll need to check them out!")
+
+
+
